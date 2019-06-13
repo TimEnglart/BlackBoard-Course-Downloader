1eff35eb599b5e40285ad9c300f5503a110dfdaf
Tim Englart <timenglart@gmail.com>
2019-06-14 02:06:56 +1000

    refactor navigation code

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def get_arguments():
     parser.add_argument("-s", "--site", help="Base Website Where Institute Black Board is Located")
     parser.add_argument("-l", "--location", help="Local Path To Save Content", default='./')
     parser.add_argument("-c", "--course", help="Course ID to Download")
-    parser.add_argument("-d", "--dump", help="Print/Dump All Course Data", action="store_true")
+    # parser.add_argument("-d", "--dump", help="Print/Dump All Course Data", action="store_true")
     parser.add_argument("-r", "--record", help="Create A Manifest For Downloaded Data", action="store_true",
                         default=True)
     parser.add_argument("-V", "--verbose", help="Print Program Runtime Information", action="store_true")
@@ -83,20 +83,11 @@ def handle_arguments():
         else:
             args.site = possible_site
 
-    if args.location is None:
-        pass
-
-    if args.course is None:
-        pass
-
-    if args.mass_download:
-        pass
-
     if args.record:
         pass
 
-    if args.dump:
-        pass
+    # if args.dump:
+    #    pass
 
     # Run Actual Program
     if args.gui:
@@ -116,7 +107,13 @@ def main(args):
     bbc = BlackBoardClient(username=args.username, password=args.password, site=args.site)
     if bbc.login():
         save_config(args)
-        navigate(bbc)
+        if args.mass_download:
+            courses = bbc.courses()  # get all Courses
+            for course in courses:
+                if args.course is None or course.id == args.course:  # Download only Specified Course
+                    course.download_all_attachments(ARGS.location)
+        else:
+            navigate(bbc)
     else:
         if input("Failed to Login [Enter 'r' to Retry]") == 'r':
             main(args)
@@ -128,92 +125,120 @@ class SubSelector:
         pass
 
 
-def current_path(path=None, **kwargs):
-    back = kwargs.get('back', False)
-    addition = kwargs.get('addition', None)
+def navigate(selected_item, path: list = None, error_message=''):
+    # Selecting New Item Based On Current Item
+    global ARGS
+    clear_console()
+    if selected_item is None:
+        raise Exception("Unknown Error")
+    next_item = None
     if path is None:
         path = []
-    if back:
-        path = path[:(-2 if 'step' not in kwargs else kwargs['step'])]
-    else:
-        if addition is not None:
-            path.append(addition)
-    return path
+    current_path(path, str(selected_item))
 
+    print("{}\n{}".format('/'.join(path), "Error: " + error_message if error_message else ""))
+    item_class_name = type(selected_item).__name__
+    if item_class_name == "BlackBoardClient":
 
-# Janky AF - unmaintainable
-def navigate(chosen_item, previous_item=None, path=None):
-    global ARGS
-    clear()
-    path = current_path(path, addition=str(chosen_item))
-    print('/'.join(
-        path) + '\n')  # + "\n{}\n-\nCURRENT: {}\nPREVIOUS: {}\n".format((chosen_item.parent_id if hasattr(chosen_item, 'parent_id') else "No Parent") if chosen_item is not None else None, chosen_item, previous_item))
-    c_type = type(chosen_item)
-    p_type = type(previous_item)
-    if c_type.__name__ == "BlackBoardClient":
-        new_item = navigation(default=None, options=chosen_item.courses(), attribute='name', sort=True, title='Courses')
-        if new_item is None:
+        # Going Forwards
+        next_item = navigation(options=selected_item.courses(), attribute='name', sort=True, title='Course')
+
+        # Going Backwards
+        if next_item is None:
             sys.exit(0)
-    elif c_type.__name__ == "BlackBoardCourse":
-        options = ["Get Content", "Download All Content"]  # Get Course Content
-        sub_selection = navigation(default=SubSelector, attribute=None, options=options)
-        if type(sub_selection).__name__ != "SubSelector":
-            index = options.index(sub_selection)
-            if index == 0:  # Content
-                new_item = navigation(default=None, options=chosen_item.contents(), attribute='title', sort=True,
-                                      title='Content')
-            elif index == 1:  # Download
-                chosen_item.download_all_attachments(ARGS.location)  # Quits When Done
-                new_item = None
-        else:
-            new_item = None
-    elif c_type.__name__ == "BlackBoardContent":
-        options = ["Get Child Content", "Get Attachments"]  # Get Child Content or Attachments
-        sub_selection = navigation(default=SubSelector, options=options, attribute=None)
-        if type(sub_selection).__name__ != "SubSelector":
-            index = options.index(sub_selection)
-            if index == 1:  # Attachments
-                new_item = navigation(default=None, options=chosen_item.attachments(), attribute='file_name', sort=True,
-                                      title='Attachment')
-                if new_item is None:
-                    navigate(chosen_item, previous_item, current_path(path, back=True, step=-1))
-            elif index == 0:  # Child Content
-                new_item = navigation(default=None, options=chosen_item.children(), attribute='title', sort=True,
-                                      title='Child')
-                if new_item is None:
-                    navigate(chosen_item, previous_item, current_path(path, back=True, step=-1))
-        else:
-            new_item = None
-    elif c_type.__name__ == "BlackBoardAttachment":
-        options = ["Download", "Back"]
-        sub_selection = navigation(default=SubSelector, options=options, attribute=None)
-        if type(sub_selection).__name__ != "SubSelector":
-            index = options.index(sub_selection)
-            if index == 0:  # Download
-                chosen_item.download(ARGS.location)
-        new_item = None
-    else:
-        new_item = None  # Go Back
-    # print(new_item.parent_id)
-    if new_item is None:
-        if previous_item is None:  # Find Parent Content
-            if c_type.__name__ == "BlackBoardContent":
-                if chosen_item.parent_id is None:
-                    navigate(chosen_item._course, None, current_path(path, back=True))
-                else:
-                    navigate(BlackBoardContent(chosen_item._course, course_id=chosen_item._course.id,
-                                               content_id=chosen_item.parent_id), None, current_path(path, back=True))
-            elif c_type.__name__ == "BlackBoardAttachment":
-                navigate(chosen_item._content, None, current_path(path, back=True))
-            elif c_type.__name__ == "BlackBoardCourse":
-                navigate(chosen_item._client, None, current_path(path, back=True))
-        else:
-            # if previous_item is None:
 
-            # else:
-            navigate(previous_item, None, current_path(path, back=True))
+    elif item_class_name == "BlackBoardCourse":
+        # Sub Selection -> New Item
+        options = ["Get Content", "Download All Content"]
+        sub_selection = navigation(options=options, title="Option")
+
+        # Going Forwards
+        if sub_selection is not None:
+            if options.index(sub_selection) == 0:  # Content
+                next_item = navigation(options=selected_item.contents(), attribute='title', sort=True,
+                                       title='Content')
+            else:  # Download
+                selected_item.download_all_attachments(ARGS.location)  # Returns None
+
+        # Going Backwards
+        if next_item is None:
+            current_path(path)
+            next_item = selected_item._client
+
+    elif item_class_name == "BlackBoardContent":
+        options = ["Get Child Content", "Get Attachments"]  # Get Child Content or Attachments
+        sub_selection = navigation(options=options, title="Option")
+
+        # Going Forward
+        if sub_selection is not None:
+            if options.index(sub_selection) == 0:  # Child Content
+                next_item = navigation(options=selected_item.children(), attribute='title', sort=True,
+                                       title='Child')
+            else:  # Attachments
+                next_item = navigation(options=selected_item.attachments(), attribute='file_name', sort=True,
+                                       title='Attachment')
+
+        # Going Backwards
+        if next_item is None:
+            current_path(path)
+            if selected_item.parent_id is None:  # Has No Parent Content (Return Course)
+                next_item = selected_item._course
+            else:  # Has Parent Content (Return Content)
+                parent_content = BlackBoardContent(selected_item._course, course_id=selected_item._course.id,
+                                                   content_id=selected_item.parent_id)
+                next_item = parent_content
+
+    elif item_class_name == "BlackBoardAttachment":
+        options = ["Download", "Back"]
+        sub_selection = navigation(options=options, title="Option")
+        if options.index(sub_selection) == 0:  # Download
+            selected_item.download(ARGS.location)
+
+        # Always Go Back to Attachments Parent
+        next_item = selected_item.content  # Originally was navigate(selected_item.content)
+
+    if next_item is not None:
+        # current_path(path)
+        navigate(next_item, path, error_message)
+    else:  # Somehow not a specified Class
+        raise Exception("Unknown Error")
+
+
+def navigation(**kwargs):
+    # Handle kwargs
+    options = kwargs.get('options', list())
+    attribute = kwargs.get('attribute', None)
+    title = kwargs.get('title', '')
+    input_text = kwargs.get('input',
+                            "Enter {} Number to Select ['c' to Exit]: ".format(title))
+    sort = kwargs.get('sort', False)
+
+    if sort and attribute is not None:
+        options = sorted(options, key=lambda element: getattr(element, attribute))
+    if not options:
+        return None  # No Options to Chose From
+    while True:
+        for i in range(len(options)):
+            print("[{}] {}".format(i + 1, getattr(options[i], attribute) if attribute is not None else str(options[i])))
+        choice = input(input_text + "\n")
+        if choice.isalpha():
+            return None  # No Choice Made
+        try:
+            choice = int(choice)
+            if choice > len(options) or choice <= 0:
+                raise ValueError
+            return options[choice - 1]
+        except TypeError:
+            print("Invalid Selection")
+        except ValueError:
+            print("Invalid Selection")
+
+
+def current_path(path: list = None, addition: str = ''):
+    if addition:
+        path.append(addition)
     else:
-        navigate(new_item, chosen_item, path)
+        del path[-2:]
 
 
 def save_config(args):
@@ -226,39 +251,7 @@ def save_config(args):
         json.dump(config, save)
 
 
-def navigation(**kwargs):
-    # Handle kwargs
-    options = kwargs.get('options', [])
-    default = kwargs.get('default', object if not options else type(options[0]))
-    title = kwargs.get('title', '')
-    input_text = kwargs.get('input',
-                            "Enter {} Number to Select ['c' to Exit]: ".format(title if title else default.__name__))
-    attribute = kwargs.get('attribute', '__name__')
-    sort = kwargs.get('sort', False)
-    previous = kwargs.get('previous', None)
-
-    if sort:
-        options = sorted(options, key=lambda element: getattr(element, attribute))
-    if not options:
-        print('No Options Present')
-        return (default() if default is not None else None) if previous is None else previous
-    while True:
-        for i in range(len(options)):
-            print("[{}] {}".format(i + 1, getattr(options[i], attribute) if attribute is not None else str(options[i])))
-        choice = input(input_text)
-        print("\n")
-        if choice.isalpha():
-            return (default() if default is not None else None) if previous is None else previous
-        try:
-            choice = int(choice)
-            if choice > len(options) or choice <= 0:
-                raise TypeError
-            return options[choice - 1]
-        except TypeError:
-            print("Invalid Selection")
-
-
-def clear():
+def clear_console():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 
